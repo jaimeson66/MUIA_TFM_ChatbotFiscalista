9c20d9ead40155361f43d0a37245eb2bc0f8b4a0
jaimeson66 <40715820+jaimeson66@users.noreply.github.com>
2024-12-26 12:28:25 +0100

    Inicio
    Inicializar el repositorio para probar en plataforma de streamlit

---
+++ src/inferencia.py
@@ -0,0 +1,36 @@
+import torch
+from transformers import T5Tokenizer, T5ForConditionalGeneration,MT5ForConditionalGeneration,AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+
+def text_gen(pregunta):
+    model_name = "timpal0l/mdeberta-v3-base-squad2"  # Modelo ya afinado en SQuAD en español
+    model_SQUAD = AutoModelForQuestionAnswering.from_pretrained(model_name)
+    tokenizer_SQUAD = AutoTokenizer.from_pretrained(model_name)
+    # Formatear la pregunta y el contexto en español
+    #pregunta = "cuando se presenta una declaracion rectificativa"
+    #pregunta = "En que modelo se informa una operacion intracomunitaria"
+    #prediccion = predictClass(pregunta)
+    #dato = df_etiquetas[df_etiquetas["Clase"] == prediccion]
+    #contexto =  dato["Respuesta"].to_string(index=False)
+    contexto = "las operaciones intracomunitarias deben informarse en el modelo 349"
+
+    inputs = tokenizer_SQUAD(
+        pregunta,
+        contexto,
+        return_tensors="pt",  # Tensores de PyTorch
+        truncation=True,      # Truncar si el texto es demasiado largo
+        max_length=512       # Longitud máxima admitida por el modelo
+    )
+    outputs = model_SQUAD(**inputs)
+
+    # Extraer las posiciones de inicio y fin con mayor probabilidad
+    start_idx = outputs.start_logits.argmax()
+    end_idx = outputs.end_logits.argmax()
+
+
+
+    respuesta = tokenizer_SQUAD.convert_tokens_to_string(
+        tokenizer_SQUAD.convert_ids_to_tokens(inputs["input_ids"][0][start_idx:end_idx + 1])
+    )
+
+    return respuesta
---
+++ src/streamlit_app.py
@@ -0,0 +1,157 @@
+import streamlit as st
+import logging
+from PIL import Image, ImageEnhance
+import time
+import torch
+import json
+import requests
+import base64
+from inferencia import text_gen
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Constants
+NUMBER_OF_MESSAGES_TO_DISPLAY = 20
+API_DOCS_URL = "https://docs.streamlit.io/library/api-reference"
+
+# Streamlit Page Configuration
+st.set_page_config(
+    page_title="Streamly - Asistente virtual de IVA",
+    layout="wide"
+
+)
+
+# Streamlit Title
+st.title("Asistente fiscal IVA")
+
+def img_to_base64(image_path):
+    """Convert image to base64."""
+    try:
+        with open(image_path, "rb") as img_file:
+            return base64.b64encode(img_file.read()).decode()
+    except Exception as e:
+        logging.error(f"Error converting image to base64: {str(e)}")
+        return None
+
+@st.cache_data(show_spinner=False)
+def long_running_task(duration):
+    """
+    Simulates a long-running operation.
+
+    Parameters:
+    - duration: int, duration of the task in seconds
+
+    Returns:
+    - str: Completion message
+    """
+    time.sleep(duration)
+    return "Long-running operation completed."
+
+@st.cache_data(show_spinner=False)
+@st.cache_data(show_spinner=False)
+
+
+def initialize_conversation():
+    """
+    Initialize the conversation history with system and assistant messages.
+
+    Returns:
+    - list: Initialized conversation history.
+    """
+    assistant_message = "¡Hola! Soy su asistente de consultas fiscales ¿En qué puedo ayudarle?"
+
+    conversation_history = [
+        {"role": "system", "content": "You are Streamly, a specialized AI assistant trained in Streamlit."},
+        {"role": "system", "content": "Streamly, is powered by the OpenAI GPT-4o-mini model, released on July 18, 2024."},
+        {"role": "system", "content": "You are trained up to Streamlit Version 1.36.0, release on June 20, 2024."},
+        {"role": "system", "content": "Refer to conversation history to provide context to your response."},
+        {"role": "system", "content": "You were created by Madie Laine, an OpenAI Researcher."},
+        {"role": "assistant", "content": assistant_message}
+    ]
+    return conversation_history
+
+@st.cache_data(show_spinner=False)
+@st.cache_data(show_spinner=False)
+#-------------ESTA FUNCION ES LA IMPORTANTE)
+def on_chat_submit(chat_input):
+    """
+    Handle chat input submissions and interact with the OpenAI API.
+    Manejar texto de entrada de usuarios para generar texto
+    Parameters:
+    - chat_input: Texto de entrada
+
+    Returns:
+    - None: Updates the chat history in Streamlit's session state.
+    """
+    user_input = chat_input.strip().lower()
+
+    #if 'conversation_history' not in st.session_state:
+    st.session_state.conversation_history = initialize_conversation()
+
+    st.session_state.conversation_history.append({"role": "user", "content": user_input})
+    assistant_reply = ""
+    assistant_reply = text_gen(user_input)
+
+
+    st.session_state.conversation_history.append({"role": "assistant", "content": assistant_reply})
+    st.session_state.history.append({"role": "user", "content": user_input})
+    st.session_state.history.append({"role": "assistant", "content": assistant_reply})
+
+
+def initialize_session_state():
+    """Initialize session state variables."""
+    if "history" not in st.session_state:
+        st.session_state.history = []
+    if 'conversation_history' not in st.session_state:
+        st.session_state.conversation_history = []
+
+def main():
+    """
+    Display Streamlit updates and handle the chat interface.
+    """
+    initialize_session_state()
+
+    if not st.session_state.history:
+        initial_bot_message = "¡Hola! Soy su asistente de consultas fiscales ¿En qué puedo ayudarle?"
+        st.session_state.history.append({"role": "assistant", "content": initial_bot_message})
+        st.session_state.conversation_history = initialize_conversation()
+
+    # Insert custom CSS for glowing effect
+    st.markdown(
+        """
+        <style>
+        .cover-glow {
+            width: 100%;
+            height: auto;
+            padding: 3px;
+            box-shadow:
+                0 0 5px #330000,
+                0 0 10px #660000,
+                0 0 15px #990000,
+                0 0 20px #CC0000,
+                0 0 25px #FF0000,
+                0 0 30px #FF3333,
+                0 0 35px #FF6666;
+            position: relative;
+            z-index: -1;
+            border-radius: 45px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    chat_input = st.chat_input("Pregunteme")
+    if chat_input:
+        on_chat_submit(chat_input)
+
+    # Display chat history
+    for message in st.session_state.history[-NUMBER_OF_MESSAGES_TO_DISPLAY:]:
+        role = message["role"]
+        avatar_image = "imgs/avatar_streamly.png" if role == "assistant" else "imgs/stuser.png" if role == "user" else None
+        with st.chat_message(role):
+            st.write(message["content"])
+
+if __name__ == "__main__":
+    main()
