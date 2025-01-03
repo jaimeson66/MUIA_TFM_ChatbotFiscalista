96377b8660d1bbd4392f342a63343c767ec4a40b
jaimeson66 <40715820+jaimeson66@users.noreply.github.com>
2025-01-03 13:47:57 +0100

    probar inferencia con modelo clasificador cargado

--- inferencia.py
+++ inferencia.py
@@ -1,18 +1,56 @@
 import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration,MT5ForConditionalGeneration,AutoModelForSeq2SeqLM
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+from transformers import BertTokenizer, BertForSequenceClassification
+import pandas as pd
+import torch
+import numpy as np
+
+# Cargar el modelo y el tokenizer
+clas_dir = "/1-ClasificadorPreguntas"
+model = BertForSequenceClassification.from_pretrained(clas_dir)
+tokenizer = BertTokenizer.from_pretrained(clas_dir)
+
+
+def clasificador_pregunta(pregunta):
+   # Tokenizar el texto
+   inputs = tokenizer(
+      input_sentence,
+      return_tensors="pt",  # Formato PyTorch
+      padding="max_length",
+      truncation=False,
+      max_length=512
+   )
+
+   # Mover los tensores al dispositivo adecuado
+   device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+   model.to(device)
+   inputs = {key: val.to(device) for key, val in inputs.items()}
+
+   # Inferencia
+   with torch.no_grad():
+      outputs = model(**inputs)
+      logits = outputs.logits
+      inferencia_clase = torch.argmax(logits, dim=-1).item()
+
+   return inferencia_clase
+
 
 def generar_respuesta(pregunta):
     model_name = "timpal0l/mdeberta-v3-base-squad2"  # Modelo ya afinado en SQuAD en español
     model_SQUAD = AutoModelForQuestionAnswering.from_pretrained(model_name)
     tokenizer_SQUAD = AutoTokenizer.from_pretrained(model_name)
+    # Importar datos desde base de conocimiento
+    df_etiquetas = pd.read_csv('/baseConocimiento.csv',encoding = 'utf-8', delimiter = ';', index_col=False)
+    dato = df_etiquetas[df_etiquetas["Clase"] == clasificador_pregunta(pregunta)]
+    contexto = dato["Respuesta"].to_string(index=False)
     # Formatear la pregunta y el contexto en español
     #pregunta = "cuando se presenta una declaracion rectificativa"
     #pregunta = "En que modelo se informa una operacion intracomunitaria"
     #prediccion = predictClass(pregunta)
     #dato = df_etiquetas[df_etiquetas["Clase"] == prediccion]
     #contexto =  dato["Respuesta"].to_string(index=False)
-    contexto = "las operaciones intracomunitarias deben informarse en el modelo 349"
+    #contexto = "las operaciones intracomunitarias deben informarse en el modelo 349"
 
     inputs = tokenizer_SQUAD(
         pregunta,
